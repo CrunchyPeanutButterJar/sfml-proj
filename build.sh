e45d4e68ed0223a2886e66410c9834c7e446953e
joe <joerustom120@gmail.com>
2025-07-28 23:28:26 +0300

    improve build script

--- build.sh
+++ build.sh
@@ -42,9 +42,22 @@ fi
 
 echo "⚙️ Build en mode $BUILD_TYPE"
 
+echo "Creation clang_profile"
+
+PROFILE_NAME="clang_profile"
+PROFILE_PATH=$(conan profile path "$PROFILE_NAME" 2>/dev/null)
+
+conan profile detect --name=$(PROFILE_NAME) --force
+
+sed -i 's/compiler=.*/compiler=clang/' "$PROFILE_PATH"
+sed -i 's/compiler.version=.*/compiler.version=18/' "$PROFILE_PATH"
+sed -i 's/compiler.libcxx=.*/compiler.libcxx=libc++/' "$PROFILE_PATH"
+sed -i 's/compiler.cppstd=.*/compiler.cppstd=23/' "$PROFILE_PATH"
+
 if [[ "$CONAN_STEP" == "true" || ! -d $BUILD_DIR ]]; then
-  conan install . \
+  CXX=clang++ CC=clang conan install . \
       --output-folder=$BUILD_DIR \
+      --profile clang_profile\
       --build=missing \
       -s build_type=$BUILD_TYPE
 fi
@@ -52,6 +65,8 @@ fi
 # Étape CMake configure + build
 cd $BUILD_DIR
 cmake .. \
+    -DCMAKE_C_COMPILER=clang\
+    -DCMAKE_CXX_COMPILER=clang++\
     -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 
