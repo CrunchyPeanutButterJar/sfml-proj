a644c87b1befe85181dcf9b16481d4cae9aabddf
joe <joerustom120@gmail.com>
2025-07-28 23:35:50 +0300

    build compile_commands.json

--- build.sh
+++ build.sh
@@ -67,6 +67,7 @@ cd $BUILD_DIR
 cmake .. \
     -DCMAKE_C_COMPILER=clang\
     -DCMAKE_CXX_COMPILER=clang++\
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON\
     -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 
