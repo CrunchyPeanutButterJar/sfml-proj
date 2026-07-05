f0724081274924fe968e56db1b418de433e0203e
joe <joerustom120@gmail.com>
2026-07-05 17:03:12 +0300

    apply clang-tidy fix

--- src/game.cpp
+++ src/game.cpp
@@ -52,8 +52,8 @@ static auto loadResolutionFromConfigFile() -> sf::Vector2u
 
 Game::Game()
     : m_window{"MyGame", loadResolutionFromConfigFile()},
-      m_stateManager{{m_window, m_window.getEventManager(), m_entityManager, m_systemManager,
-                      m_textureManager}},
+      m_stateManager{{.m_window=m_window, .m_eventManager=m_window.getEventManager(), .m_entityManager=m_entityManager, .m_systemManager=m_systemManager,
+                      .m_textureManager=m_textureManager}},
       m_entityManager{m_systemManager, m_stateManager.getContext().m_textureManager},
       m_systemManager{m_entityManager}
 {
